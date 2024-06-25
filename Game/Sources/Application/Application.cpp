d4dfc414d6b58524b7d306266ab364f42b8e74cf
haruhi130 <kd1341678@st.kobedenshi.ac.jp>
2024-06-25 14:00:17 +0900

    コメント追加

--- Game/Sources/Application/Application.cpp
+++ Game/Sources/Application/Application.cpp
@@ -2,12 +2,14 @@
 
 void Application::Execute()
 {
+	// ウィンドウ作成
 	if (!m_window.Create(1280, 720, L"FrameworkDX12", L"Window"))
 	{
 		assert(0 && "ウィンドウ作成失敗");
 		return;
 	}
 
+	// ゲームループ
 	while (true)
 	{
 		if (!m_window.ProcessMessage())
